3cf5290eebf273c3558b392ec851643a01af30d8
israel navarrete <navarrete.alvarado@gmail.com>
2012-06-09 16:02:28 -0500

    Codigos ejemplos, videotutorias 3,4

---
+++ Prueba/src/Prueba/android/apis/PruebaActivity.java
@@ -0,0 +1,13 @@
+package Prueba.android.apis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PruebaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ SrSombrero/src/com/videotutoriales/srsombrero/SrSombreroActivity.java
@@ -0,0 +1,13 @@
+package com.videotutoriales.srsombrero;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SrSombreroActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
