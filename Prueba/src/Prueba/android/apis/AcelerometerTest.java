ffcc4a6d6fff5423a68802505e75595b4043a660
israel navarrete <navarrete.alvarado@gmail.com>
2012-06-11 20:57:46 -0500

    video tutrial 7  acelerometro

---
+++ Prueba/src/Prueba/android/apis/AcelerometerTest.java
@@ -0,0 +1,49 @@
+package Prueba.android.apis;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class AcelerometerTest extends Activity implements SensorEventListener {
+	TextView textView;
+	StringBuilder builer = new StringBuilder();
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		textView = new TextView(this);
+		setContentView(textView);
+		SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		if (manager.getSensorList(Sensor.TYPE_ACCELEROMETER).size()==0) {
+			textView.setText("No hay hacelerometro instalado");
+		}else {
+			Sensor accelerometer = manager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+			if (!manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)) {
+				textView.setText("No se ha podido registrar el sensor listener");
+			}
+		}
+	}
+	
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		builer.setLength(0);
+		builer.append("x: ");
+		builer.append(event.values[0]);
+		builer.append(", y: ");
+		builer.append(event.values[1]);
+		builer.append(", z: ");
+		builer.append(event.values[2]);
+		textView.setText(builer.toString());
+	}
+	
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 TODO
 ==
-falta cometar codigo del single touch, multitouch, key 
-hacer mas ejemplos de multitouc, key test, explorar la apli
+falta cometar codigo del single touch, multitouch, key, acelometro 
+hacer mas ejemplos de multitouc, key test, explorar la apli, acelerometro 
 
 
