744fea3cd2c18c1b8383739ad178b54823860a8e
israel navarrete <navarrete.alvarado@gmail.com>
2012-06-13 15:02:47 -0500

    video 9 sond poll conpleto

--- Prueba/src/Prueba/android/apis/SoundPollTest.java
+++ Prueba/src/Prueba/android/apis/SoundPollTest.java
@@ -1,17 +1,45 @@
 package Prueba.android.apis;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.TextView;
 
-public class SoundPollTest extends Activity {
-	StringBuilder builder = new StringBuilder();
-	TextView textview;
+public class SoundPollTest extends Activity implements OnTouchListener{
+	SoundPool soundPoll;
+	int misonido =-1;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		textview = new TextView(this);
-		textview.setText("No lista");
-		setContentView(textview);
+		TextView textView = new TextView(this);
+		textView.setOnTouchListener(this);
+		setContentView(textView);
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		soundPoll = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+		try {
+			AssetManager assetManager = getAssets();
+			AssetFileDescriptor descriptor  = assetManager.openFd("sound37.ogg");
+			misonido = soundPoll.load(descriptor, 1);
+			
+		} catch (IOException e) {
+			textView.setText("No se ha podido cargar el efecto de sonido"+e.getMessage());
+		} 
 	}
-}
+	public boolean onTouch(View view, MotionEvent event) {
+		if (event.getAction()==MotionEvent.ACTION_UP) {
+			if (misonido!=-1) {
+				soundPoll.play(misonido, 1, 1, 0, 0, 1);
+				
+			}
+		}
+		return true;
+	}
+} 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 TODO
 ==
-falta cometar codigo del single touch, multitouch, key, acelometro 
-hacer mas ejemplos de multitouc, key test, explorar la apli, acelerometro 
+falta cometar codigo de viarias activitys 
+hacer mas ejemplos de multitouc, key test, explorar la apli, acelerometro
 revisar videos http://youtu.be/5P525BRLH9A, http://youtu.be/xBjbbWwpucE, http://youtu.be/M1sS9Zkm5is
 
 
